f884fbd53b78b4ea86905838c036b1dc67b3eabc
leehah <leehah0908@naver.com>
2024-08-09 17:50:39 +0900

    day1

---
+++ src/begin/StartAll.java
@@ -0,0 +1,22 @@
+package begin;
+
+public class StartAll {
+    public static void main(String[] args) {
+        int i = 10;
+        long L = 2323451231242323L;
+        double d = 3.14123123123213;
+        float f = 3.1412312312313123131F;
+        char c = 'L';
+        String str = "LHH";
+        boolean b = true;
+
+        System.out.println("int = " + i);
+        System.out.println("long = " + L);
+        System.out.println("double = " + d);
+        System.out.println("float = " + f);
+        System.out.println("char = " + c);
+        System.out.println("string = " + str);
+        System.out.println("boolean = " + b);
+
+    }
+}
